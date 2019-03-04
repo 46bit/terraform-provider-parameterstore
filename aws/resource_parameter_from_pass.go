001623a59c0f9fe2e7503294affc697ea071ca5a
Michael Mokrysz <hi@46b.it>
2019-03-04 09:18:05 +0000

    WIP

--- aws/resource_parameter_from_pass.go
+++ aws/resource_parameter_from_pass.go
@@ -1,7 +1,9 @@
 package aws
 
 import (
+	"encoding/hex"
 	"fmt"
+	"golang.org/x/crypto/scrypt"
 	"log"
 	"os"
 	"os/exec"
@@ -52,6 +54,26 @@ func resourceParameterFromPass() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
+			// We use a cryptographic hash of the decrypted value from `pass` as a fingerprint
+			// of whether the secret in `pass` has changed between Terraform runs. This means that
+			// a cryptographic hash of the secret value goes into the Terraform state files.
+			// scrypt was chosen because it is suitable for storing encrypted passwords and
+			// provides a fixed output for a fixed input (unlike bcrypt.)
+			"scrypt_of_value_in_pass": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			// We use changes to the Last Modified time of the Parameter in Parameter Store
+			// to detect changes since the last Terraform run. This was chosen for two reasons:
+			//
+			// - It does not require decrypting secrets from Parameter Store, which (e.g.)
+			//   reduces event noise in CloudTrail;
+			// - Encrypted values returned from Parameter Store change constantly and so cannot
+			//   be used to fingerprint of the secret's value.
+			"last_modified_in_parameterstore": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 		},
 	}
 }
@@ -75,25 +97,24 @@ func resourceParameterFromPassExists(d *schema.ResourceData, meta interface{}) (
 func resourceParameterFromPassRead(d *schema.ResourceData, meta interface{}) error {
 	ssmconn := meta.(*AWSClient).ssmconn
 
-	log.Printf("[DEBUG] Reading SSM Parameter: %s", d.Id())
-
-	resp, err := ssmconn.GetParameter(&ssm.GetParameterInput{
-		Name:           aws.String(d.Id()),
-		WithDecryption: aws.Bool(true),
-	})
+	secret, err := fetchSecretFromPass(d.Get("pass_dir").(string), d.Get("pass_key").(string))
 	if err != nil {
-		return fmt.Errorf("error getting SSM parameter: %s", err)
+		return fmt.Errorf("error fetching `pass` secret: %s", err)
 	}
+	scryptBytes, err := scrypt.GenerateFromPassword([]byte(secret), 10)
+	if err != nil {
+		return fmt.Errorf("error bcrypting `pass` secret: %s", err)
+	}
+	d.Set("bcrypt_of_value_in_pass", hex.EncodeToString(bcryptBytes))
 
-	param := resp.Parameter
-	d.Set("parameter_name", param.Name)
+	log.Printf("[DEBUG] Reading Metadata of SSM Parameter: %s", d.Id())
 
 	describeParamsInput := &ssm.DescribeParametersInput{
 		ParameterFilters: []*ssm.ParameterStringFilter{
 			{
 				Key:    aws.String("Name"),
 				Option: aws.String("Equals"),
-				Values: []*string{aws.String(d.Get("parameter_name").(string))},
+				Values: []*string{aws.String(d.Id())},
 			},
 		},
 	}
@@ -109,8 +130,10 @@ func resourceParameterFromPassRead(d *schema.ResourceData, meta interface{}) err
 	}
 
 	detail := describeResp.Parameters[0]
+	d.Set("parameter_name", detail.Name)
 	d.Set("key_id", detail.KeyId)
 	d.Set("description", detail.Description)
+	d.Set("last_modified_in_parameterstore", detail.LastModifiedDate.String())
 
 	arn := arn.ARN{
 		Partition: meta.(*AWSClient).partition,
@@ -142,22 +165,17 @@ func resourceParameterFromPassDelete(d *schema.ResourceData, meta interface{}) e
 func resourceParameterFromPassPut(d *schema.ResourceData, meta interface{}) error {
 	ssmconn := meta.(*AWSClient).ssmconn
 
-	log.Printf("[INFO] Creating SSM Parameter: %s", d.Get("parameter_name").(string))
-
-	passDir := d.Get("pass_dir").(string)
-	passKey := d.Get("pass_key").(string)
-	log.Printf("[DEBUG] Fetching secret '%s' from Pass directory '%s'", passKey, passDir)
-	cmd := exec.Command("pass", passKey)
-	cmd.Env = append(os.Environ(), fmt.Sprintf("PASSWORD_STORE_DIR=%s", passDir))
-	output, err := cmd.Output()
+	secret, err := fetchSecretFromPass(d.Get("pass_dir").(string), d.Get("pass_key").(string))
 	if err != nil {
-		return fmt.Errorf("error fetching from pass: %s / %s", err, err.(*exec.ExitError).Stderr)
+		return err
 	}
+
+	log.Printf("[INFO] Creating SSM Parameter: %s", d.Get("parameter_name").(string))
 
 	paramInput := &ssm.PutParameterInput{
 		Name:      aws.String(d.Get("parameter_name").(string)),
 		Type:      aws.String("SecureString"),
-		Value:     aws.String(string(output)),
+		Value:     aws.String(secret),
 		Overwrite: aws.Bool(true),
 	}
 
@@ -179,4 +197,27 @@ func resourceParameterFromPassPut(d *schema.ResourceData, meta interface{}) erro
 	d.SetId(d.Get("parameter_name").(string))
 
 	return resourceParameterFromPassRead(d, meta)
+}
+
+func fetchSecretFromPass(passDir, passKey string) (string, error) {
+	log.Printf("[DEBUG] Fetching '%s' from `pass` '%s'", passKey, passDir)
+	cmd := exec.Command("pass", passKey)
+	if len(passDir) > 0 {
+		cmd.Env = append(os.Environ(), fmt.Sprintf("PASSWORD_STORE_DIR=%s", passDir))
+	}
+	output, err := cmd.Output()
+	if err != nil {
+		// FIXME: That this error is an `*exec.ExitError` is an implementation detail;
+		// code this more defensively.
+		return "", fmt.Errorf("%s / %s", err, err.(*exec.ExitError).Stderr)
+	}
+	return string(output), nil
+}
+
+func fetchScryptedSecretFromPass(passDir, passKey) (string, error) {
+	secret, err := fetchSecretFromPass(passDir, passKey)
+	if err != nil {
+		return "", err
+	}
+	scrypt.Key()
 }
